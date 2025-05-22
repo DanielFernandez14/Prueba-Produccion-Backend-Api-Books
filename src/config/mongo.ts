4ee5a23f78da389dd99e999326afcb47e9f163ec
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-05-22 16:45:20 -0300

    prueba produccion arreglo dotenv 4 debug

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -7,8 +7,10 @@ const URI_DB = process.env.URI_DB || ""
 const connectMongoDB = async () => {
     try {
         await connect(URI_DB)
+        
         console.log("✅ Conectado correctamente a MongoDB")
     } catch (error) {
+        console.log(error)
         console.log(`📛 no se pudo conectar a MongoDB`)
     }
 }
